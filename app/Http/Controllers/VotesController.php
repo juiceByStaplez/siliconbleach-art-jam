c5622a733e0202d7d1d8e42c8b1b905ff056936f
Matthew McKey <me@matthewmckey.com>
2020-07-28 13:24:31 -0700

    Add Log class reference to VotesController

--- app/Http/Controllers/VotesController.php
+++ app/Http/Controllers/VotesController.php
@@ -6,6 +6,7 @@ use App\Vote;
 use App\User;
 use Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Database\QueryException;
 
 class VotesController extends Controller
